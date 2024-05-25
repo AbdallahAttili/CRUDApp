89e649c9ffe6fe3455ff3d917d7a9030600b72e6
AbdallahAttili <abdallahattili@gmail.com>
2024-05-25 17:38:27 +0300

    adding everything at the same time like an idiot

---
+++ the-first-task/src/main/java/com/forbes/task/the/first/task/TheFirstTaskApplication.java
@@ -0,0 +1,12 @@
+package com.forbes.task.the.first.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFirstTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheFirstTaskApplication.class, args);
+    }
+}
